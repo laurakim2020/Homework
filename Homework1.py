7af30c21f55e68606c90ef29e8a92fea9ac76fa8
laurakim2020 <31958630+laurakim2020@users.noreply.github.com>
2017-12-04 21:07:56 -0500

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,13 @@
+def turn_clockwise(d):
+    if d=="N": 
+        return "E"
+    elif d=="E": 
+        return "S"
+    elif d=="S": 
+        return "W"
+    elif d== "W": 
+        return "N"
+    else:
+        return "None"
+    
+    
