e3b27f728c2ad933a903436964aad7af1d0baa0b
laurakim2020 <laurakim2020@gmail.com>
2018-02-24 10:24:09 -0500

    Car Race
    Car Race

--- pygame/car.race.py
+++ pygame/car.race.py
@@ -9,8 +9,11 @@ display_height = 600
 
 black = (0, 0, 0)
 white = (255, 255, 255)
-red = (255, 0, 0)
+red = (200, 0, 0)
 green = (0,200,0)
+
+bright_red = (255,0,0)
+bright_green = (0,255,0)
 
 block_color = (53,115,255)
 
@@ -70,11 +73,15 @@ def game_intro():
         TextRect.center = ((display_width / 2), (display_height / 2))
         gameDisplay.blit(TextSurf, TextRect)
 
+        mouse = pygame.mouse.get_pos()
 
-        pygame.draw.rect(gameDisplay, green, (150, 450, 100, 50))
+        #print(mouse)
+
+        if 150 + 100 > mouse[0] > 150 and 450 + 50 > mouse[1] > 450:
+            pygame.draw.rect(gameDisplay, bright_green, (150, 450, 100, 50))
+        else:
+            pygame.draw.rect(gameDisplay, green, (150, 450, 100, 50))
         pygame.draw.rect(gameDisplay, red, (550, 450, 100, 50))
-
-
         pygame.display.update()
         clock.tick(15)
 
