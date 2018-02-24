8c9c2fef1c3eb4346cbe80f5f909b4450e067cf8
laurakim2020 <laurakim2020@gmail.com>
2018-02-24 11:02:56 -0500

    Car Game
    Car Racing Game

--- pygame/car.race.py
+++ pygame/car.race.py
@@ -56,11 +56,14 @@ def message_display(text):
 def crash():
     message_display('You Crashed')
 
-def button(msg,x,y,w,h,ic,ac):
+def button(msg,x,y,w,h,ic,ac,action=None):
     mouse = pygame.mouse.get_pos()
-
+    click = pygame.mouse.get_pressed()
+    print(click)
     if x + w > mouse[0] > x and y + h > mouse[1] > y:
         pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
+        if click[0] == 1 and action != None:
+            action()
     else:
         pygame.draw.rect(gameDisplay, ic, (x, y, w, h))
 
@@ -68,6 +71,10 @@ def button(msg,x,y,w,h,ic,ac):
     textSurf, textRect = text_objects(msg, smallText)
     textRect.center = ((x + (w / 2)), (y + (h / 2)))
     gameDisplay.blit(textSurf, textRect)
+
+def quitgame():
+    pygame.quit
+    quit()
 
 def game_intro():
     intro = True
@@ -80,13 +87,13 @@ def game_intro():
                 quit()
 
         gameDisplay.fill(white)
-        largeText = pygame.font.Font('freesansbold.ttf', 115)
+        largeText = pygame.font.SysFont("comicsansms",115)
         TextSurf, TextRect = text_objects("A bit Racey", largeText)
         TextRect.center = ((display_width / 2), (display_height / 2))
         gameDisplay.blit(TextSurf, TextRect)
 
-        button("GO!", 150, 450, 100, 50, green, bright_green)
-        button("Quit!", 550, 450, 100, 50, red, bright_red)
+        button("GO!", 150, 450, 100, 50, green, bright_green, game_loop)
+        button("Quit!", 550, 450, 100, 50, red, bright_red,quitgame)
 
 
 
