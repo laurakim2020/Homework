dc20f30cf968973e1f5eaef9dc693e9b9e359488
laurakim2020 <laurakim2020@gmail.com>
2018-03-29 12:40:39 -0400

    sbhw

---
+++ SpringBreakHW/Chapter 9.py
@@ -0,0 +1,14 @@
+def tuple((a,b), (c,d)):
+    if (a+b)*(c+d) >= 25:
+        print("greater than or equal to 25")
+    else:
+        print("less than 25")
+
+print(tuple(3,4), (12,6))
+
+
+
+
+
+
+
